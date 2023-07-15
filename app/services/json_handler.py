e57c28c78e0432cd254e5846a85aa6633f03c8fd
dafedin <tm.i.sm.master@gmail.com>
2023-07-15 20:25:04 +0300

    Json allocated
    Json file saved to source folder

--- app/services/json_handler.py
+++ app/services/json_handler.py
@@ -1,15 +1,28 @@
+import os.path
+
 import requests
+
+from app.config import FILES_INPUT_DIR
 from app.loggers.logger import get_logger
 
 json_path = 'http://api.open-notify.org/astros.json'
 
 
-def get_request():
-    return requests.get(json_path)
+def get_response():
+    def save_file():
+        with open(os.path.join(FILES_INPUT_DIR, 'astros.json'), 'wb') as file:
+            for _ in request.iter_content(chunk_size=8192):
+                file.write(_)
+
+    request = requests.get(json_path)
+
+    save_file()
+
+    return request
 
 
-def get_response() -> dict | None:
-    response = get_request()
+def get_response_data() -> dict | None:
+    response = get_response()
     logger = get_logger()
 
     if response.status_code == 200:
@@ -22,8 +35,7 @@ def get_response() -> dict | None:
 
 
 def print_astros_number() -> None:
-    
-    json_dict = get_response()
+    json_dict = get_response_data()
     number_key = 'number'
 
     if 'number' in json_dict.keys():
