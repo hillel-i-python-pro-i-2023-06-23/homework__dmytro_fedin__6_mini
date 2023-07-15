2beef44fae35371b482fe327b58053ee7de2c4dc
dafedin <tm.i.sm.master@gmail.com>
2023-07-15 19:41:33 +0300

    Astros got

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from app.services import print_output
-from app.services.json_handler import get_response
+from app.services.json_handler import print_astros_number
 from app.services.user_csv_printer import write_csv
 
 
 def main():
     print_output('some_input.txt')
     write_csv()
-    get_response()
+    print_astros_number()
--- app/services/json_handler.py
+++ app/services/json_handler.py
@@ -1,23 +1,32 @@
 import requests
-
 from app.loggers.logger import get_logger
 
 json_path = 'http://api.open-notify.org/astros.json'
 
 
-def get_json():
+def get_request():
     return requests.get(json_path)
 
 
-def get_response():
-
-    response = get_json()
+def get_response() -> dict | None:
+    response = get_request()
     logger = get_logger()
 
     if response.status_code == 200:
         json_data = response.json()
         logger.info(f'Response successful in {json_path}')
-        # print(json_data['number'])
-        print(json_data['number'])
+        return json_data
     else:
         logger.info(f'Error in getting response with code {response.status_code}')
+        return None
+
+
+def print_astros_number() -> None:
+    
+    json_dict = get_response()
+    number_key = 'number'
+
+    if 'number' in json_dict.keys():
+        print(f'Number of astros is {json_dict[number_key]}')
+    else:
+        print('The direct number of astros is unknown but can be got')
