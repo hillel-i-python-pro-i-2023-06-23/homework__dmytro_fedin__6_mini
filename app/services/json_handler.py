fbb694aad4c3f14be3acbf28e9db354b74866f58
dafedin <tm.i.sm.master@gmail.com>
2023-07-15 21:04:38 +0300

    Request handling optimized
    Request by url moved to responses.py

--- app/services/json_handler.py
+++ app/services/json_handler.py
@@ -1,41 +1,22 @@
-import os.path
+from app.services.responses import get_response
 
-import requests
-
-from app.config import FILES_INPUT_DIR
-from app.loggers.logger import get_logger
-
-json_path = 'http://api.open-notify.org/astros.json'
+json_url = 'http://api.open-notify.org/astros.json'
 
 
-def get_response():
-    def save_file():
-        with open(os.path.join(FILES_INPUT_DIR, 'astros.json'), 'wb') as file:
-            for _ in request.iter_content(chunk_size=8192):
-                file.write(_)
-
-    request = requests.get(json_path)
-
-    save_file()
-
-    return request
-
-
-def get_response_data() -> dict | None:
-    response = get_response()
-    logger = get_logger()
+def get_json_data() -> dict | None:
+    response = get_response(json_url)
 
     if response.status_code == 200:
         json_data = response.json()
-        logger.info(f'Response successful in {json_path}')
+
         return json_data
     else:
-        logger.info(f'Error in getting response with code {response.status_code}')
+
         return None
 
 
 def print_astros_number() -> None:
-    json_dict = get_response_data()
+    json_dict = get_json_data()
     number_key = 'number'
 
     if 'number' in json_dict.keys():
