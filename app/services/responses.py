1ffc7bb941935e16d1b0ec3bcd91b536bdc8eae2
dafedin <tm.i.sm.master@gmail.com>
2023-07-16 20:49:40 +0300

    Commented items deleted
    Unused saving and parameters links.

--- app/services/responses.py
+++ app/services/responses.py
@@ -2,7 +2,6 @@ import os
 import requests
 from requests import Response
 
-from app.config import FILES_INPUT_DIR
 from urllib.parse import urlsplit
 from app.loggers.logger import get_logger
 
@@ -16,12 +15,6 @@ def get_filename_from_url(url=None) -> str | None:
     return file_name
 
 
-# def save_file(request, file_path) -> None:
-#     with open(file_path, 'wb') as file:
-#         for item in request.iter_content(chunk_size=8192):
-#             file.write(item)
-
-
 def get_response(url: str) -> Response | None:
     # file_name = get_filename_from_url(url)
 
@@ -30,9 +23,6 @@ def get_response(url: str) -> Response | None:
     try:
         request = requests.get(url)
         logger.info(f'Request successful in {url}')
-
-        # file_path = os.path.join(FILES_INPUT_DIR, file_name)
-        # save_file(request, file_path)
 
         return request
 
