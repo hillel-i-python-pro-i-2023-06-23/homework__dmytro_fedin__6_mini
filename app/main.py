83f8f2974fd14f096c2e85a4fcad8f2533f7ee0c
dafedin <tm.i.sm.master@gmail.com>
2023-07-16 10:26:09 +0300

    Main annotation
    Added to main file.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ from app.services.user_csv_printer import write_csv
 
 
 def main():
+    # 1. Read some file.
     print_output('some_input.txt')
+    # 2. Generate users.
     write_csv()
+    # 3. Who's here?
     print_astros_number()
+    # 4. Print CSV data.
     print_csv_data()
